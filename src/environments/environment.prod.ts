2d9470fc3c518f3d6f5d0ff1c951e8ff4f4f5b90
Ilias Musin <icilias.musin@americanwell.com>
2023-06-04 00:46:35 +0200

    patch prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://a0gnzu1g01.execute-api.us-east-1.amazonaws.com/prod',
+    order: 'https://a0gnzu1g01.execute-api.us-east-1.amazonaws.com/prod',
+    import: 'https://a0gnzu1g01.execute-api.us-east-1.amazonaws.com/prod',
+    bff: 'https://a0gnzu1g01.execute-api.us-east-1.amazonaws.com/prod',
+    cart: 'https://a0gnzu1g01.execute-api.us-east-1.amazonaws.com/prod',
   },
   apiEndpointsEnabled: {
     product: true,
